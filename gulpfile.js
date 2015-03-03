3f78ee431fbbeb463b52a43af8b71785e242a40e
Stewart Duffy <stewart.duffy@elgin.org.uk>
2015-03-04 08:12:47 +1300

    Adds compass-dev gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var uglify      = require('gulp-uglify');
 var concat      = require('gulp-concat');
 var compass     = require('gulp-compass');
 var minifyCSS   = require('gulp-minify-css');
-var minifyCSS   = require('gulp-copy');
 
 var src = {
     scss: 'app/scss/*.scss',
@@ -20,20 +19,34 @@ var src = {
 };
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['compass'], function() {
+gulp.task('serve', ['compass-dev'], function() {
     browserSync({
         server: "./app"
     });
 
-    gulp.watch(src.scss, ['compass']);
+    gulp.watch(src.scss, ['compass-dev']);
     gulp.watch(src.html).on('change', reload);
+});
+
+gulp.task('compass-dev', function() {
+    gulp.src(src.scss)
+        .pipe(compass({
+            sass: 'app/scss',
+            css: src.css,
+        }))
+        .on('error', function(err) {
+            console.log(err);
+        })
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
 });
 
 gulp.task('compass', function() {
     gulp.src(src.scss)
         .pipe(compass({
             sass: 'app/scss',
-            css: src.css,
+            css: 'app/dist/css',
         }))
         .on('error', function(err) {
             console.log(err);
@@ -55,7 +68,6 @@ gulp.task('copy', function() {
       .pipe(gulp.dest('app/dist/'));
 });
 
-
-gulp.task('build', ['scripts', 'copy']);
+gulp.task('build', ['scripts', 'copy', 'compass']);
 
 gulp.task('default', ['serve']);
