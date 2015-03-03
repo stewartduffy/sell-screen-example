eb6d79dad737c881366557c1f93fd173973bca8b
Stewart Duffy <stewart.duffy@elgin.org.uk>
2015-03-04 00:19:56 +1300

    Adds calculations for GST & Totals.

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,7 @@
     var app = angular.module('store', []);
 
     app.controller('StoreController', function($scope) {
-        this.salesItems = defaultSalesItems || [];
+        this.salesItems = [];
         $scope.addSalesItem = function () {
             var newSalesItem = {
                 quantity: 1,
@@ -11,31 +11,79 @@
                 exclPrice: 260.86956,
                 taxRate: 0.15
             };
+            newSalesItem.inclPrice = $scope.addGst(newSalesItem.exclPrice, newSalesItem.taxRate);
+            newSalesItem.taxPrice = $scope.calculateGst(newSalesItem.exclPrice, newSalesItem.taxRate);
+
+
             $scope.store.salesItems.push(newSalesItem);
         };
+
+        
+        $scope.calculateGst = function(price, taxRate) {
+            return (price * (taxRate * 100) / 100).toFixed(2);
+        };
+        
+        $scope.addGst = function(price, taxRate) {
+            return (price * 1 + ( price * (taxRate * 100) / 100 )).toFixed(2);
+        };
+
+        $scope.getTaxTotal = function(){
+            var total = 0;
+            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+
+            for(var i = 0; i < $scope.store.salesItems.length; i++){
+                var salesItem = $scope.store.salesItems[i];
+                total += (salesItem.taxPrice * salesItem.quantity);
+            }
+            return total.toFixed(2);
+        };
+        
+        $scope.getExclTotal = function(){
+            var total = 0;
+            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+
+            for(var i = 0; i < $scope.store.salesItems.length; i++){
+                var salesItem = $scope.store.salesItems[i];
+                total += (salesItem.exclPrice * salesItem.quantity);
+            }
+            return total.toFixed(2);
+        };
+
+        $scope.getInclTotal = function(){
+            var total = 0;
+            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+
+            for(var i = 0; i < $scope.store.salesItems.length; i++){
+                var salesItem = $scope.store.salesItems[i];
+                total += (salesItem.inclPrice * salesItem.quantity);
+            }
+            return total.toFixed(2);
+        };
+
+
     });
 
-    //default data
-    var defaultSalesItems = [
-        {
-            quantity: 1,
-            name: 'Eternal Teeshirt',
-            exclPrice: 260.86956,
-            taxRate: 0.15
-        },
-        {
-            quantity: 1,
-            name: 'Eternal Teeshirt',
-            exclPrice: 260.86956,
-            taxRate: 0.15
-        },
-        {
-            quantity: 1,
-            name: 'Eternal Teeshirt',
-            exclPrice: 260.86956,
-            taxRate: 0.15
-        }
-    ];
+    //defaultSalesItems data
+    // var defaultSalesItems = [
+    //     {
+    //         quantity: 1,
+    //         name: 'Eternal Teeshirt',
+    //         exclPrice: 260.86956,
+    //         taxRate: 0.15
+    //     },
+    //     {
+    //         quantity: 1,
+    //         name: 'Eternal Teeshirt',
+    //         exclPrice: 260.86956,
+    //         taxRate: 0.15
+    //     },
+    //     {
+    //         quantity: 1,
+    //         name: 'Eternal Teeshirt',
+    //         exclPrice: 260.86956,
+    //         taxRate: 0.15
+    //     }
+    // ];
 
 
 })();
