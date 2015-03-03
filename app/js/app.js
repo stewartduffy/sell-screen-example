5804477cdac98051fb5ee8df79cd154e48619d0a
Stewart Duffy <stewart.duffy@elgin.org.uk>
2015-03-04 00:36:12 +1300

    Changes store controller to salesScreen controller

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
     var app = angular.module('store', []);
 
-    app.controller('StoreController', function($scope) {
+    app.controller('SalesScreenController', function($scope) {
         this.salesItems = [];
         $scope.addSalesItem = function () {
             var newSalesItem = {
@@ -14,13 +14,13 @@
             newSalesItem.inclPrice = $scope.addGst(newSalesItem.exclPrice, newSalesItem.taxRate);
             newSalesItem.taxPrice = $scope.calculateGst(newSalesItem.exclPrice, newSalesItem.taxRate);
 
-            $scope.store.salesItems.push(newSalesItem);
+            $scope.salesScreen.salesItems.push(newSalesItem);
         };
 
         $scope.resetSalesItems = function () {
             var confirmMessage = "You are about to remove all the products from the cart!\r\n 'Cancel' to stop, 'OK' to continue.";
-            if ($scope.store.salesItems.length && window.confirm(confirmMessage)) {
-                $scope.store.salesItems = [];
+            if ($scope.salesScreen.salesItems.length && window.confirm(confirmMessage)) {
+                $scope.salesScreen.salesItems = [];
             }
         };
 
@@ -33,7 +33,7 @@
             };
             newSalesItem.inclPrice = $scope.addGst(newSalesItem.exclPrice, newSalesItem.taxRate);
             newSalesItem.taxPrice = $scope.calculateGst(newSalesItem.exclPrice, newSalesItem.taxRate);
-            $scope.store.salesItems.push(newSalesItem);
+            $scope.salesScreen.salesItems.push(newSalesItem);
         };
         
         $scope.calculateGst = function(price, taxRate) {
@@ -46,10 +46,10 @@
 
         $scope.getTaxTotal = function(){
             var total = 0;
-            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+            console.log("$scope.salesScreen.salesItems = ", $scope.salesScreen.salesItems);
 
-            for(var i = 0; i < $scope.store.salesItems.length; i++){
-                var salesItem = $scope.store.salesItems[i];
+            for(var i = 0; i < $scope.salesScreen.salesItems.length; i++){
+                var salesItem = $scope.salesScreen.salesItems[i];
                 total += (salesItem.taxPrice * salesItem.quantity);
             }
             return total.toFixed(2);
@@ -57,10 +57,10 @@
         
         $scope.getExclTotal = function(){
             var total = 0;
-            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+            console.log("$scope.salesScreen.salesItems = ", $scope.salesScreen.salesItems);
 
-            for(var i = 0; i < $scope.store.salesItems.length; i++){
-                var salesItem = $scope.store.salesItems[i];
+            for(var i = 0; i < $scope.salesScreen.salesItems.length; i++){
+                var salesItem = $scope.salesScreen.salesItems[i];
                 total += (salesItem.exclPrice * salesItem.quantity);
             }
             return total.toFixed(2);
@@ -68,10 +68,10 @@
 
         $scope.getInclTotal = function(){
             var total = 0;
-            console.log("$scope.store.salesItems = ", $scope.store.salesItems);
+            console.log("$scope.salesScreen.salesItems = ", $scope.salesScreen.salesItems);
 
-            for(var i = 0; i < $scope.store.salesItems.length; i++){
-                var salesItem = $scope.store.salesItems[i];
+            for(var i = 0; i < $scope.salesScreen.salesItems.length; i++){
+                var salesItem = $scope.salesScreen.salesItems[i];
                 total += (salesItem.inclPrice * salesItem.quantity);
             }
             return total.toFixed(2);
@@ -79,28 +79,5 @@
 
 
     });
-
-    //defaultSalesItems data
-    // var defaultSalesItems = [
-    //     {
-    //         quantity: 1,
-    //         name: 'Eternal Teeshirt',
-    //         exclPrice: 260.86956,
-    //         taxRate: 0.15
-    //     },
-    //     {
-    //         quantity: 1,
-    //         name: 'Eternal Teeshirt',
-    //         exclPrice: 260.86956,
-    //         taxRate: 0.15
-    //     },
-    //     {
-    //         quantity: 1,
-    //         name: 'Eternal Teeshirt',
-    //         exclPrice: 260.86956,
-    //         taxRate: 0.15
-    //     }
-    // ];
-
 
 })();
